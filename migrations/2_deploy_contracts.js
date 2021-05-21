474452eafb80f74b827069888cb8f417f563b577
Pierre Filliolaud <pierre.filliolaud@gmail.com>
2021-05-22 00:06:52 +0200

    Fixing escrow address initialisation

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,6 +5,7 @@ module.exports = function(deployer) {
   deployer.then(async () => {
     this.myToken = await deployer.deploy(MyToken, 'MyToken', 'SIM', 10000);
     this.escrow = await deployer.deploy(Escrow, MyToken.address);
+    await this.myToken.initEscrowAddress(this.escrow.address)
     await this.escrow.initMyTokenAddress(this.myToken.address)
     console.log(Escrow.address)
     console.log(MyToken.address)
